77d09e3f2f8ad673ab8fb140c847c9b3812bccf8
dkleiner <dkleiner@7db4f970-e477-432e-b09d-acd7789d73f1>
2008-03-20 21:19:29 +0000

    Resizing of dialog done later in creation process

--- JPEGView/FileOpenDialog.h
+++ JPEGView/FileOpenDialog.h
@@ -10,13 +10,13 @@ public:
 	BEGIN_MSG_MAP(CFileOpenDialog)
 		MESSAGE_HANDLER(WM_INITDIALOG, OnInitDialog)
 		MESSAGE_HANDLER(MYWM_POSTINIT, OnPostInitDialog)
-		NOTIFY_CODE_HANDLER(CDN_SELCHANGE, OnSelChange)
 	END_MSG_MAP()
 
 	LRESULT OnInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
 	LRESULT OnPostInitDialog(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/);
-	LRESULT OnSelChange(int /*idCtrl*/, LPNMHDR pnmh, BOOL& /*bHandled*/);
 
 private:
+	void SizeDialog();
+
 	static bool m_bSized;
 };
