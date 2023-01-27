32056a8296847f77323cc7bc352f68f589ffc4b2
Kevin M <sylikc@gmail.com>
2023-01-27 14:51:26 -0800

    first cut of routines.  working build of libwebp and replace

---
+++ extras/scripts/vs-init.bat
@@ -0,0 +1,37 @@
+@echo off
+
+REM this is a routine that would detect and initialize visual studio environment.
+REM It's not going to be tested very well for new versions because I only have VS2017 and VS2019 Community installed
+
+IF /I "%~1" EQU "" (
+	echo ERROR: Pass in an [arch] to be passed to a vcvarsall.bat
+	exit /b 1
+)
+
+SET XVS_BASE_COMMON=%ProgramFiles(x86)%\Microsoft Visual Studio
+
+IF NOT EXIST "%XVS_BASE_COMMON%" (
+	echo Visual Studio not installed or installed in a non-standard location!
+	exit /b 1
+)
+
+SET XVS_VCVARS_BAT=
+
+REM because the directories in theory should come out in order
+REM it'll automatically pick up the LAST vcvarsall.bat (in theory)
+FOR /F "usebackq tokens=*" %%I IN (`dir /b /on /s "%XVS_BASE_COMMON%\vcvarsall.bat"`) DO (
+	echo + Found VCVARSALL.BAT at: %%I
+	SET XVS_VCVARS_BAT=%%I
+)
+
+
+echo == Using: %XVS_VCVARS_BAT% ==
+call "%XVS_VCVARS_BAT%" %~1
+
+
+
+SET XVS_BASE_COMMON=
+SET XVS_VCVARS_BAT=
+
+
+exit /b 0
