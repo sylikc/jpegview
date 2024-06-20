e75cea9021e4a5ab454621f291014919780d7bfa
qbnu <93988953+qbnu@users.noreply.github.com>
2024-06-19 23:37:06 -0400

    use TJ3 API for decoding

--- src/JPEGView/TJPEGWrapper.cpp
+++ src/JPEGView/TJPEGWrapper.cpp
@@ -17,23 +17,23 @@ void * TurboJpeg::ReadImage(int &width,
 	nchannels = 3;
 	chromoSubsampling = TJSAMP_420;
 
-	tjhandle hDecoder = tjInitDecompress();
+	tjhandle hDecoder = tj3Init(TJINIT_DECOMPRESS);
 	if (hDecoder == NULL) {
 		return NULL;
 	}
 
 	unsigned char* pPixelData = NULL;
-	int nSubSampling;
-	int nResult = tjDecompressHeader2(hDecoder, (unsigned char*)buffer, sizebytes, &width, &height, &nSubSampling);
-	if (nResult == 0){
-		chromoSubsampling = (TJSAMP)nSubSampling;
+	int nResult = tj3DecompressHeader(hDecoder, (unsigned char*)buffer, sizebytes);
+	if (nResult == 0) {
+		width = tj3Get(hDecoder, TJPARAM_JPEGWIDTH);
+		height = tj3Get(hDecoder, TJPARAM_JPEGHEIGHT);
+		chromoSubsampling = (TJSAMP)tj3Get(hDecoder, TJPARAM_SUBSAMP);
 		if (abs((double)width * height) > MAX_IMAGE_PIXELS) {
 			outOfMemory = true;
 		} else if (width <= MAX_IMAGE_DIMENSION && height <= MAX_IMAGE_DIMENSION) {
 			pPixelData = new(std::nothrow) unsigned char[TJPAD(width * 3) * height];
 			if (pPixelData != NULL) {
-				nResult = tjDecompress2(hDecoder, (unsigned char*)buffer, sizebytes, pPixelData, width, TJPAD(width * 3), height,
-					TJPF_BGR, 0);
+				nResult = tj3Decompress8(hDecoder, (unsigned char*)buffer, sizebytes, pPixelData, TJPAD(width * 3), TJPF_BGR);
 				if (nResult != 0) {
 					delete[] pPixelData;
 					pPixelData = NULL;
@@ -44,7 +44,7 @@ void * TurboJpeg::ReadImage(int &width,
 		}
 	}
 
-	tjDestroy(hDecoder);
+	tj3Destroy(hDecoder);
 
 	return pPixelData;
 }
